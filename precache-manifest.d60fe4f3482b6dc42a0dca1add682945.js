64c05e1b0f4054b788b734d6d458e65dbf374a2b
Halkim44 <kimketchum15@gmail.com>
2019-05-21 14:53:56 +0800

    Updates

---
+++ precache-manifest.d60fe4f3482b6dc42a0dca1add682945.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1da63fe2eadd5104ab04c218554359",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "3cfd6a28adb9ebc42a14",
+    "url": "/react-weather-app/static/css/main.a0d17986.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/react-weather-app/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "3cfd6a28adb9ebc42a14",
+    "url": "/react-weather-app/static/js/main.4b09e3ce.chunk.js"
+  },
+  {
+    "revision": "e736fc5812ad96aa92b3",
+    "url": "/react-weather-app/static/js/runtime~main.7e96bc80.js"
+  }
+]);
